66cbf6ea86a5877e8c985a057776c40e7eca8d66
nguyenloan <20020114@vnu.edu.vn>
2023-01-13 16:59:23 +0700

    base project and header of main page

---
+++ lib/home/food_page_body.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class FoodPageBody extends StatefulWidget {
+  const FoodPageBody({Key? key}) : super(key: key);
+
+  @override
+  _FoodPageBodyState createState() => _FoodPageBodyState();
+}
+
+class _FoodPageBodyState extends State<FoodPageBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class AppColors {
+  static final Color textColor = const Color(0xFFccc7c5);
+  static final Color mainColor = const Color(0xFF2196F3);
+  static final Color iconColor1 = const Color(0xFFffd28d);
+  static final Color iconColor2 = const Color(0xFFfcab88);
+  static final Color paraColor = const Color(0xFF8f837f);
+  static final Color buttonBgColor = const Color(0xFFffffff);
+  static final Color signColor = const Color(0xFFa9a29f);
+  static final Color mainBlackColor = const Color(0xFF332d2b);
+  static final Color yellowColor = const Color(0xFFffd379);
+}
